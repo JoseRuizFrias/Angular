34a323c28cb9ebd22c862a5348a7887efb0f746f
Jose <jose.r.frias88@gmail.com>
2023-03-28 14:02:10 +0200

    firstCommit

---
+++ src/api/mock-data.gallery.ts
@@ -0,0 +1,44 @@
+import { ApiPictureEntity } from "./api.model";
+
+export const apiPictureList: ApiPictureEntity[] = [
+  {
+    id: "4kk33",
+    title: "bernabeu",
+    src: "assets/gallery/fondo_1.jpg",
+  },
+  {
+    id: "5rt329",
+    title: "escudo_corazon",
+    src: "assets/gallery/fondo_2.jpg",
+  },
+  {
+    id: "jd782j",
+    title: "bernabeu_fondo",
+    src: "assets/gallery/fondo_3.jpg",
+  },
+  {
+    id: "kd82j",
+    title: "hasta_el_final",
+    src: "assets/gallery/fondo_4.jpg",
+  },
+  {
+    id: "0lokw2",
+    title: "modric_gold",
+    src: "assets/gallery/fondo_5.jpg",
+  },
+  {
+    id: "d83j2u",
+    title: "rm_galaxy",
+    src: "assets/gallery/fondo_6.jpg",
+  },
+  {
+    id: "idueh43",
+    title: "templo_futbol",
+    src: "assets/gallery/fondo_7.jpg",
+  },
+  {
+    id: "0i7gx1",
+    title: "vini_jr",
+    src: "assets/gallery/fondo_8.png",
+  },
+];
